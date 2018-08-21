e1d39051c6ea5974a0a8258076c096c84dacf153
hljlishen <31783394+hljlishen@users.noreply.github.com>
2018-08-21 23:38:53 +0800

    添加项目文件。

---
+++ ListOperator/ListOperator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOperator
+{
+    public class ListOperator<T>
+    {
+        private List<T> list;
+
+        public ListOperator(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public List<T> SubList(int beginIndex, int endIndex) => SubList(list, beginIndex, endIndex);
+        public static List<T> SubList(List<T> originalList, int beginIndex, int endIndex, int step = 1)
+        {
+            if (originalList == null) return null;
+            if (originalList.Count == 0) return new List<T>();
+            if (beginIndex > endIndex || beginIndex < 0 || endIndex < 0 || beginIndex >= originalList.Count) throw new Exception("下标值传递错误");
+            if(step <= 0) throw new Exception("step值应该为正整数");
+
+            List<T> subList = new List<T>();
+            endIndex = Math.Min(endIndex, originalList.Count - 1);
+
+            for(; beginIndex <= endIndex; beginIndex += step)
+            {
+                subList.Add(originalList[beginIndex]);
+            }
+
+            return subList;
+        }
+        public bool ValueEqual(List<T> ls) => ValueEqual(list, ls);
+        public static bool ValueEqual(List<T> firstList, List<T> secondList)
+        {
+            if (firstList == null && secondList != null || firstList != null && secondList == null) return false;
+            if (firstList == null && secondList == null) throw new Exception("两个List全为null");
+            if (firstList.Count != secondList.Count) return false;
+            if (firstList.Count == 0 && secondList.Count == 0) return true;
+            if (!(firstList[0] is IComparable)) throw new Exception(typeof(T).ToString() + "类型不是IComparable");
+
+            for(int idx = 0; idx < firstList.Count; idx++)
+            {
+                IComparable firstValue = (IComparable)firstList[idx];
+                IComparable secondValue = (IComparable)secondList[idx];
+                if (firstValue.CompareTo(secondValue) != 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ListOperatorTests/ListOperatorTests.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ListOperator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOperator.Tests
+{
+    [TestClass()]
+    public class ListOperatorTests
+    {
+        [TestMethod()]
+        public void TestNormal()
+        {
+            List<int> ls1 = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+            List<int> ls2 = ListOperator<int>.SubList(ls1, 3, 7);
+            List<int> ls3 = new List<int>(new int[] { 4, 5, 6, 7, 8 });
+
+            Assert.AreEqual(ListOperator<int>.ValueEqual(ls3, ls2), true);
+        }
+
+        [TestMethod()]
+        public void TestLengthBiggerThanCount()
+        {
+            List<int> ls1 = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            List<int> ls2 = ListOperator<int>.SubList(ls1, 3, 11);
+            List<int> ls3 = new List<int>(new int[] { 4, 5, 6, 7, 8, 9, 10 });
+
+            Assert.AreEqual(ListOperator<int>.ValueEqual(ls3, ls2), true);
+        }
+
+        [TestMethod()]
+        public void TestDiffLengthAreEqual()
+        {
+            List<int> ls1 = new List<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+            List<int> ls2 = new List<int>(new int[] { 1, 2, 3, 4, 5});
+
+            bool expected = false;
+            bool actual = ListOperator<int>.ValueEqual(ls1, ls2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void TestSingleNull()
+        {
+            List<int> ls1 = null;
+            List<int> ls2 = new List<int>(new int[] { 1, 2, 3, 4, 5 });
+
+            bool expected = false;
+            bool actual = ListOperator<int>.ValueEqual(ls1, ls2);
+            Assert.AreEqual(expected, actual);
+
+            ls1 = new List<int>();
+            ls2 = null;
+            Assert.AreEqual(ListOperator<int>.ValueEqual(ls1, ls2), false);
+        }
+
+        [TestMethod()]
+        public void TestDoubleEmpty()
+        {
+            List<int> ls1 = new List<int>();
+            List<int> ls2 = new List<int>();
+
+            bool expected = true;
+            bool actual = ListOperator<int>.ValueEqual(ls1, ls2);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void TestDiffValueAreEqual()
+        {
+            List<int> ls1 = new List<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+            List<int> ls2 = new List<int>(new int[] { 1, 2, 3, 4, 5, 7 });
+
+            bool expected = false;
+            bool actual = ListOperator<int>.ValueEqual(ls1, ls2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void TestSameValueAreEqual()
+        {
+            List<int> ls1 = new List<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+            List<int> ls2 = new List<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+
+            bool expected = true;
+            bool actual = ListOperator<int>.ValueEqual(ls1, ls2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void TestDoubleSameValueAreEqual()
+        {
+            List<double> ls1 = new List<double>(new double[] { 1.00001, 2, 3, 4, 5, 6 });
+            List<double> ls2 = new List<double>(new double[] { 1.00001, 2, 3, 4, 5, 6 });
+
+            bool expected = true;
+            bool actual = ListOperator<double>.ValueEqual(ls1, ls2);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
